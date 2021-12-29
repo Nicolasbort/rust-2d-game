938949dc67135020c4f63430982c447d185cfe4c
Nicolas Bortoluzzi <bortoluzzinicolas@gmail.com>
2021-12-29 14:51:46 -0300

    Removing unused method

--- src/point.rs
+++ src/point.rs
@@ -4,12 +4,6 @@ pub struct Point {
     pub x: f64,
 }
 
-impl Default for Point {
-    fn default() -> Point {
-        Point { x: 0.0, y: 0.0 }
-    }
-}
-
 impl Point {
     pub fn new(x: f64, y: f64) -> Self {
         Point { x, y }
